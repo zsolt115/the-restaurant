0c1947e666b48a088ee87ad5e8104b4b3fdad58d
zsolt115 <sikozsolt96@gmail.com>
2025-12-15 00:35:52 +0200

    home page, new image/design, hero section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,14 +3,16 @@ import styles from "./home.module.scss";
 export default function HomePage() {
   return (
     <main className={styles.homePage}>
-      <h1 className={styles.title_h1}>Welcome to The Restaurant</h1>
-      <h2 className={styles.title_h2}>
-        Authentic flavors, crafted with passion.
-      </h2>
-      <hr />
-      {/* <section key={section.category} className={styles.section}>
-        <h2>{section.category}</h2>
-      </section> */}
+      <section className={styles.hero}>
+        <div className={styles.overlay} />
+
+        <div className={styles.heroContent}>
+          <h1 className={styles.title_h1}>Welcome to The Restaurant</h1>
+          <h2 className={styles.title_h2}>
+            Authentic flavors, crafted with passion.
+          </h2>
+        </div>
+      </section>
     </main>
   );
 }
